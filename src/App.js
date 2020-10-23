75bdfc637b1e5348efca34141290626478050194
Ravi <ravidesaai@gmail.com>
2020-10-23 08:48:46 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar,Nav,NavDropdown } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">CONDORS COMMUNITY</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#features">Post</Nav.Link>
+      <Nav.Link href="#pricing">Feeds</Nav.Link>
+      <NavDropdown title="People" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Find</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Make Post</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Activity</NavDropdown.Item>
+        <NavDropdown.Divider />
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#deets">Profile</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+      Groups
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     </div>
   );
 }
